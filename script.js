569e0abcf4f8eb971c15c3a56630cc8b8df6c635
JHuang0309 <jhuang.study@gmail.com>
2023-06-30 15:38:13 +1000

    About Me flexes with emoji icons

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const header = document.querySelector(".navbar")
 // change navbar to dark background after a certain point
 window.onscroll = function() {
     var top = window.scrollY;
-    if(top >=300) {
+    if(top >=480) {
         // change background to white and change text to dark 
         header.classList.add('navbarWhite');
         header.classList.remove('navbar-dark');
